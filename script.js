9753533a45e97c5c87ba381e1776497931d00d1c
Ma3tin <martin.mares@educanet.cz>
2022-01-03 23:10:04 +0100

    Hotovo DOPICE

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ window.onload = function () {
     let button = document.getElementById("button")
     button.addEventListener("click", function () {
         let emailInput = document.getElementById("email")
-        if (emailInput.value.length > 3) document.getElementById("rightEmail").innerText = "nice"
+        let rightEmail = document.getElementById("rightEmail")
+        if (emailInput.value.includes("@")) {
+            rightEmail.className = "valid"
+            rightEmail.innerText = "Looks good!"
+        } else {
+            rightEmail.className = "invalid"
+            rightEmail.innerText = "Please provide valid email."
+        }
+        let passwordInput = document.getElementById("password")
+        let rightPassword = document.getElementById("rightPassword")
+        if (passwordInput.value.length >= 8 && passwordInput.value.match(/[A-Z]{1,}/g)) {
+            rightPassword.className = "valid"
+            rightPassword.innerText = "Looks good!"
+        } else {
+            rightPassword.className = "invalid"
+            rightPassword.innerText = "Please provide valid password."
+        }
     })
 }
